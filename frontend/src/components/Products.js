77d743109d7827265d20636e5a9be62cf7bbeb7d
José <jlfb55@gmail.com>
2021-05-07 16:15:55 -0400

    Ratings y Product Card

---
+++ frontend/src/components/Products.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Ratings from "./Ratings";
+
+const Products = (props) => {
+  
+  const {product} = props;
+
+  return (
+    <div key={product._id} className="card">
+      <a href={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </a>
+      <div className="card-body">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <Ratings 
+          rating={product.rating}          
+        />
+        <div className="price">${product.price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
