50772a3b330538cf2776adc096510fdfe58053e9
José <jlfb55@gmail.com>
2021-05-15 16:20:26 -0400

    Add to Cart - Funcional

--- frontend/src/services/store.js
+++ frontend/src/services/store.js
@@ -1,14 +1,22 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "../reducers/cartReducers";
 import {
   productDetailsReducer,
   productListReducer,
 } from "../reducers/productReducers";
 
-const initialState = {};
+const initialState = {
+  cart:{
+    cartItems: localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -20,3 +28,4 @@ const store = createStore(
 );
 
 export default store;
+ 
