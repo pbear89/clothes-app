40587e09258c612d6624d165eaecafb868cdd08a
José <jlfb55@gmail.com>
2021-06-07 13:58:01 -0400

    ShippingScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import CartScreen from "./pages/CartScreen";
 import HomeScreen from "./pages/HomeScreen";
 import ProductScreen from "./pages/ProductScreen";
 import RegisterScreen from "./pages/RegisterScreen";
+import ShippingAddressScreen from "./pages/ShippingAddressScreen";
 import SigninScreen from "./pages/SigninScreen";
 
 
@@ -21,6 +22,7 @@ function App() {
         <Route path="/product/:id" component={ProductScreen} ></Route>
         <Route path="/signin" component={SigninScreen}></Route>
         <Route path="/register" component={RegisterScreen}></Route>
+        <Route path="/shipping" component={ShippingAddressScreen}></Route>
       </main>
       
       <footer className="row center">All right reserved</footer>
